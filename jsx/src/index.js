531231a8b1a0484e9e3cb6e3a53302d4a1f48498
jsmile <jsmile@naver.com>
2020-01-30 21:08:22 +0900

    jsx variable

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -2,6 +2,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function getButtonText() { return 'Click on me'; }
+
 // Create a react component. 
 // const App = function() 
 // {
@@ -9,11 +11,21 @@ import ReactDOM from 'react-dom';
 // }
 const App = () => 
 {
+  const labelText = 'Enter your name : ';
+  const buttonStyle = { backgroundColor: 'orange', color: 'dark-gray' };
+  const buttonText1 = 'Click me 1 !';               // javascript String
+  const buttonText2 = { text: 'Click me 2 !' };   // javascript object
+  
+
   return (
     <div>
-      <label class="label" for="name">Enter you name</label>
+      <label className="label" htmlFor="name">{labelText}</label>
       <input id="name" type="text" />
-      <button style={{ backgroundColor: 'orange', color: 'white' }}>submit</button>
+      <button style={buttonStyle}>
+        {getButtonText()}
+        {buttonText1}
+        {buttonText2.text}
+      </button>
     </div>
   );
   // classical expression of html
@@ -23,7 +35,7 @@ const App = () =>
   //   <button style="background-color: orange; color: white">submit</button>
   //   <button style={{ backgroundColor: 'orange', color: 'white' }}>submit</button>
   // </div>;
-};
+};  // end of App
 
 // Take the react component and show it on the screen.
 ReactDOM.render( <App />, document.querySelector( '#root' ) );
